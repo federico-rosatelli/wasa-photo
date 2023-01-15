e35888d8c5758d988eb68f940063338d6b8dce76
federico-rosatelli <frosatelli@gmail.com>
2023-01-15 15:14:42 +0100

    Custom error fixed for StatusError

--- service/api/customErrors/handleErrors.go
+++ service/api/customErrors/handleErrors.go
@@ -1,15 +1,43 @@
 package customErrors
 
-type ErrStatus struct {
+type StatusError struct {
 	message string
 }
 
 func NewErrStatus(message string) error {
-	errStruct := &ErrStatus{
+	errStruct := &StatusError{
 		message: message,
 	}
 	return errStruct
 }
-func (e *ErrStatus) Error() string {
+func (e *StatusError) Error() string {
 	return e.message
 }
+
+// This setup could be used for the costum error with the type of the error.
+// e.g. an error with typeError equal to 0 would be an internalstatuserror.
+// The function Type is used like a switch case for the type error so you can delete
+// the const variables in the file routing-functions and use only the int value
+// type StatusError struct {
+// 	message   string
+// 	typeError int
+// }
+
+// type error interface {
+// 	Error() string
+// 	Type() int
+// }
+
+// func NewErrStatus(message string) error {
+// 	errStruct := &StatusError{
+// 		message: message,
+// 	}
+// 	return errStruct
+// }
+// func (e *StatusError) Error() string {
+// 	return e.message
+// }
+
+// func (e *StatusError) Type() int {
+// 	return e.typeError
+// }
